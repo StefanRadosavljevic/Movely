684b643d5abe9d53c7cf6cb559f6e7c5cf0c5b0b
Stefan Radosavljevic <stefke.mail@gmail.com>
2021-08-08 05:24:50 +0200

    added search filed in topbar widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,10 +6,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class HomePage extends ConsumerWidget {
   double _deviceHeight;
   double _deviceWidth;
+
+  TextEditingController _searchTextFieldController;
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+
+    _searchTextFieldController = TextEditingController();
+
     return _buildUI();
   }
 
@@ -23,6 +28,7 @@ class HomePage extends ConsumerWidget {
           alignment: Alignment.center,
           children: [
             _backgroundWidget(),
+            _foregroundWidget(),
           ],
         ),
       ),
@@ -46,6 +52,62 @@ class HomePage extends ConsumerWidget {
         filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
         child: Container(
           decoration: BoxDecoration(color: Colors.black.withOpacity(0.7)),
+        ),
+      ),
+    );
+  }
+
+  Widget _foregroundWidget() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.fromLTRB(0, _deviceHeight * 0.02, 0, 0),
+        width: _deviceWidth * 0.88,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _topBarWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return Container(
+      height: _deviceHeight * 0.08,
+      decoration: BoxDecoration(
+        color: Colors.white38,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Row(
+        children: [
+          _searchFieldWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _searchFieldWidget() {
+    final _border = InputBorder.none;
+    return Container(
+      width: _deviceWidth * 0.50,
+      height: _deviceHeight * 0.50,
+      child: Center(
+        child: TextField(
+          controller: _searchTextFieldController,
+          onSubmitted: (_input) {},
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            focusedBorder: _border,
+            border: _border,
+            prefixIcon: Icon(Icons.search, color: Colors.white24),
+            hintStyle: TextStyle(color: Colors.white54),
+            filled: false,
+            fillColor: Colors.white24,
+            hintText: 'Search for a movie',
+          ),
         ),
       ),
     );
