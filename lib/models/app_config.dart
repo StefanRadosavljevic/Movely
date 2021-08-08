d2e21731a41e020e9d748abc42c6bafaf60f6ae3
Stefan Radosavljevic <stefke.mail@gmail.com>
2021-08-08 04:30:41 +0200

    Made app config globally availible through app.

---
+++ lib/models/app_config.dart
@@ -0,0 +1,17 @@
+class AppConfig {
+  // ignore: non_constant_identifier_names
+  final String API_KEY;
+  // ignore: non_constant_identifier_names
+  final String BASE_API_URL;
+  // ignore: non_constant_identifier_names
+  final String BASE_IMAGE_API_URL;
+
+  AppConfig({
+    // ignore: non_constant_identifier_names
+    this.API_KEY,
+    // ignore: non_constant_identifier_names
+    this.BASE_API_URL,
+    // ignore: non_constant_identifier_names
+    this.BASE_IMAGE_API_URL,
+  });
+}
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:moviesapp/models/app_config.dart';
 
 class SplashPage extends StatefulWidget {
+  //when splashscreen is finished this callback will be called
   final VoidCallback onInitComplete;
   const SplashPage(
       {Key key, @required this.onInitComplete, Null Function() oninitComplete})
@@ -15,7 +21,28 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 2)).then((_) => widget.onInitComplete());
+    // when config values are extracted init is completed
+    Future.delayed(Duration(seconds: 2)).then(
+      (_) => _setup(context).then(
+        (_) => widget.onInitComplete(),
+      ),
+    );
+  }
+
+  // get config file for api
+  Future<void> _setup(BuildContext context) async {
+    final getIt = GetIt.instance;
+
+    final configFile = await rootBundle.loadString('assets/config/main.json');
+    // decode json file into data
+    final configData = jsonDecode(configFile);
+
+    //register AppConfig with getit(still dont understand what this thing is doing)
+    getIt.registerSingleton<AppConfig>(AppConfig(
+      API_KEY: configData['API_KEY'],
+      BASE_API_URL: configData['BASE_API_URL'],
+      BASE_IMAGE_API_URL: configData['BASE_IMAGE_API_URL'],
+    ));
   }
 
   @override
