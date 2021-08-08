f1808c0878e50a0197ae6c42a8b6d9cba4b48497
Stefan Radosavljevic <stefke.mail@gmail.com>
2021-08-08 03:52:59 +0200

    created splash screen

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'pages/splash_page.dart';
+
+void main() {
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitComplete: () {
+        return null;
+      },
+    ),
+  );
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitComplete;
+  const SplashPage(
+      {Key key, @required this.onInitComplete, Null Function() oninitComplete})
+      : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movely',
+      home: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/logo.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
